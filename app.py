adc1c561b852d1071cc55986d8f7f6137e943482
Christian Lowe <christglowe@gmail.com>
2018-03-05 09:27:47 -0600

    Checked to see if this thing could tell the difference between me and Khalid

--- app.py
+++ app.py
@@ -3,22 +3,31 @@ import dlib
 from PIL import Image
 import face_recognition
 
-image = face_recognition.load_image_file("./known/meAndBoo.jpg")
+known_image = face_recognition.load_image_file("./known/me.jpg")
+unknown_image = face_recognition.load_image_file("./known/me2.jpg")
 
 
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
+christianencoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-print("I found {} face(s) in this photograph.".format(len(face_locations)))
+results = face_recognition.compare_faces([christianencoding], unknown_encoding)
 
-for face_location in face_locations:
 
-    # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+print(results)
 
-    # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
+
+
+# face_locations = face_recognition.face_locations(image)
+#
+# print("I found {} face(s) in this photograph.".format(len(face_locations)))
+#
+# for face_location in face_locations:
+#
+#     # Print the location of each face in this image
+#     top, right, bottom, left = face_location
+#     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+#
+#     # You can access the actual face itself like this:
+#     face_image = image[top:bottom, left:right]
+#     pil_image = Image.fromarray(face_image)
+#     pil_image.show()
